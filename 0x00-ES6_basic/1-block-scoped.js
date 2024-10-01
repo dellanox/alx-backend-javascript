970df4ef1b65e85f21cd28882ce34b5318fae021
Your Name <you@example.com>
2024-10-01 15:08:21 +0000

    edit: remove comments 1-block-scoped.js

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,11 +1,11 @@
 export default function taskBlock(trueOrFalse) {
-  const task = false;  // Use const for variables that won't be reassigned
+  const task = false;
   const task2 = true;
 
   if (trueOrFalse) {
-    const task = true;   // Block-scoped variables using const
+    const task = true;
     const task2 = false;
   }
 
-  return [task, task2];  // Return the outer task and task2 variables
+  return [task, task2];
 }
