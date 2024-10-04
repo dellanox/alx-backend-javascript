3697201320f130935332e20af09199a999ec7e1b
Your Name <you@example.com>
2024-10-04 01:08:27 +0000

    10-car.js

---
+++ 0x02-ES6_classes/10-car.js
@@ -0,0 +1,18 @@
+const cloneSymbol = Symbol('clone');
+
+export default class Car {
+  constructor(brand, motor, color) {
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
+  }
+
+  // Method to clone the car
+  [cloneSymbol]() {
+    return new this.constructor(this._brand, this._motor, this._color);
+  }
+
+  cloneCar() {
+    return this[cloneSymbol]();
+  }
+}
