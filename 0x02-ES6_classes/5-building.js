0da54963bdbd9bf6171412402ff58d78074893e9
Your Name <you@example.com>
2024-10-04 00:39:11 +0000

    5-building.js

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,18 @@
+export default class Building {
+  constructor(sqft) {
+    if (typeof sqft !== 'number') {
+      throw new TypeError('Square footage must be a number');
+    }
+    this._sqft = sqft;
+
+    // Ensure that any class extending Building implements evacuationWarningMessage
+    if (this.constructor !== Building && this.evacuationWarningMessage === undefined) {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
+  }
+
+  // Getter for sqft
+  get sqft() {
+    return this._sqft;
+  }
+}
